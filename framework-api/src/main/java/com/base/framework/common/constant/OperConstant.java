e7c38bc1fb1060ee7a7f028d3972c7dfb56369ff
zhouyw <279704064@qq.com>
2018-05-15 21:48:11 +0800

    常量-操作

---
+++ framework-api/src/main/java/com/base/framework/common/constant/OperConstant.java
@@ -0,0 +1,19 @@
+package com.base.framework.common.constant;
+
+/**
+ * OperConstant
+ * @author zhouyw
+ * @date 2018.05.15
+ */
+public interface OperConstant {
+
+    String TRUE = "1";
+
+    String FALSE = "0";
+
+    String ADD = "I";
+
+    String EDIT = "U";
+
+    String DEL = "D";
+}
